9ec7d2166d2caeeb8a396dfdc2ca5a21038f32d6
TERESIA012 <kingoriteresia@gmail.com>
2021-08-22 21:29:23 +0300

    modify class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,11 @@
 export class Quotes {
+    showQuote: boolean;
+    dislike: any;
+      constructor(public index:number ,public name: string, public quote: string, public musician: string,public posted:Date) {
+          this.showQuote=false;
+          // this.posted=new Date;
+      }
+
+
+
 }
